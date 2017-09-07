a5285dbac438ad8649965f63592d96fe11e47d22
ALONDRA COTRINA <ALONDRA COTRINA@ALONDRACOTRINA>
2017-09-06 22:09:13 -0500

    first

---
+++ src/exercise_01_01/Exercise_01_01.java
@@ -0,0 +1,12 @@
+/* (Display three messages) Write a program that displays Welcome to Java,
+   Welcome to Computer Science, and Programming is fun.
+*/
+package exercise_01_01;
+public class Exercise_01_01 {
+     public static void main(String[] args) {
+         System.out.println("Welcome to Java");
+         System.out.println("Welcome to Computer Science");
+         System.out.println("Programming is fun");
+    }
+}  
+
